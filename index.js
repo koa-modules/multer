4b0bef74228a62806f683297d6aa5a688a5e6b94
iooly <zhangshaolin0279@gmail.com>
2016-04-18 04:18:58 +0800

    reformat code

--- index.js
+++ index.js
@@ -14,48 +14,48 @@ const multerFields = ['file', 'files', 'body'];
 const originalMulter = require('multer');
 
 function multer(options) {
-	const m = originalMulter(options);
+  const m = originalMulter(options);
 
-	const _makeMiddleware = m._makeMiddleware.bind(m);
-	m._makeMiddleware = makePromise(_makeMiddleware);
+  const _makeMiddleware = m._makeMiddleware.bind(m);
+  m._makeMiddleware = makePromise(_makeMiddleware);
 
-	const any = m.any.bind(m);
-	m.any = makePromise(any);
+  const any = m.any.bind(m);
+  m.any = makePromise(any);
 
-	return m
+  return m
 }
 
 function makePromise(fn) {
-	return (fields, fileStrategy) => {
-		return function *(next) {
-			yield promise(this);
-			yield next;
-		};
-		function promise(ctx) {
-			return new Promise((resolve, reject) => {
-				fn(fields, fileStrategy)(ctx.req, ctx.res, (err) => {
-					if (err) {
-						reject(err)
-					} else {
-						copyFields(ctx);
-						resolve(ctx)
-					}
-				})
-			});
-		}
-	}
+  return (fields, fileStrategy) => {
+    return function *(next) {
+      yield promise(this);
+      yield next;
+    };
+    function promise(ctx) {
+      return new Promise((resolve, reject) => {
+        fn(fields, fileStrategy)(ctx.req, ctx.res, (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            copyFields(ctx);
+            resolve(ctx)
+          }
+        })
+      });
+    }
+  }
 }
 
 function copyFields(ctx) {
-	let req = ctx.req;
-	let len = multerFields.length;
-	let field;
-	for (let i = 0; i < len; i ++) {
-		field = multerFields[i];
-		if (req.hasOwnProperty(field)) {
-			ctx[field] = req[field];
-		}
-	}
+  let req = ctx.req;
+  let len = multerFields.length;
+  let field;
+  for (let i = 0; i < len; i ++) {
+    field = multerFields[i];
+    if (req.hasOwnProperty(field)) {
+      ctx[field] = req[field];
+    }
+  }
 }
 
 multer.diskStorage = originalMulter.diskStorage;
