bbcdfaf22d8d9acb858daf1635191a69a78be361
Techborn <techborn@users.noreply.github.com>
2017-05-02 12:59:26 -0500

    Forgot to expose the single file reference.

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ function makePromise(multer, name) {
       return new Promise((resolve, reject) => {
         middleware(ctx.req, ctx.res, (err) => {
           if (ctx.req.body) ctx.request.body = ctx.req.body
+          if (ctx.req.file) ctx.request.file = ctx.req.file
           if (ctx.req.files) ctx.request.files = ctx.req.files
           err ? reject(err) : resolve(ctx)
         })
